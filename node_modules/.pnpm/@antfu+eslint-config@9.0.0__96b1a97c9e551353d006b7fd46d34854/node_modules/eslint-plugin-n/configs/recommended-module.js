3f8fd142669483aa1f63b6b230a19170a8573b7f
Wayne <chenweiwool@163.com>
2026-08-14 15:39:00 +0800

    chore: 依赖

--- node_modules/.pnpm/@antfu+eslint-config@9.0.0__96b1a97c9e551353d006b7fd46d34854/node_modules/eslint-plugin-n/configs/recommended-module.js
+++
@@ -1,8 +0,0 @@
-/**
- * @fileoverview the `recommended-module` config for `eslint.config.js`
- * @author 唯然<weiran.zsd@outlook.com>
- * @deprecated use `flat/recommended-module` instead
- */
-
-import pkg from '../lib/index.js';
-export default pkg.configs["flat/recommended-module"]
--- node_modules/.pnpm/@antfu+eslint-config@9.0.0__96b1a97c9e551353d006b7fd46d34854/node_modules/eslint-plugin-n/configs/recommended-script.js
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview the `recommended-script` config for `eslint.config.js`
- * @author 唯然<weiran.zsd@outlook.com>
- * @deprecated use `flat/recommended-script` instead
- */
-
-
-import pkg from '../lib/index.js';
-export default pkg.configs["flat/recommended-script"]
